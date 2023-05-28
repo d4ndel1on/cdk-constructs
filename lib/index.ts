b61a3b70106d47a6a1e6f42eccd7336ac47fe3f2
d4ndel1on <stefan@fancynetwork.net>
2023-05-28 11:05:31 +0200

    change export

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,6 @@
 import {ApiGatewayServedLambda} from "./ApiGatewayServedLambda";
 import {HostedReactApp} from "./HostedReactApp";
 
-export * from './HostedReactApp'
-export * from './ApiGatewayServedLambda'
-
 export {
   HostedReactApp,
   ApiGatewayServedLambda,
